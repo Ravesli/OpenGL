84c1e9b6d805d97bf366dac0c23f0ce6d0bdcff7
donButadeus <dezzines@gmail.com>
2020-06-15 00:38:53 +0300

    Новый урок

--- Урок 21. Смешивание в OpenGL/Исходный код №2 - Урок 21. Смешивание в OpenGL/Hello_Window/main.cpp
+++ Урок 21. Смешивание в OpenGL/Исходный код №2 - Урок 21. Смешивание в OpenGL/Hello_Window/main.cpp
@@ -349,9 +349,16 @@ unsigned int loadTexture(char const* path)
         glBindTexture(GL_TEXTURE_2D, textureID);
         glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	
+	  //для этого урока: используйте GL_CLAMP_TO_EDGE, вместо выделенных ниже строчек, чтобы предотвратить появление полупрозрачных границ. 
+	  //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT); 
+      //glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, format == GL_RGBA ? GL_CLAMP_TO_EDGE : GL_REPEAT);
+	 
+      //------------------------------------------------------------------//
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);    //
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);   //
+	  //---------------------------------------------------------------//
+      
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
