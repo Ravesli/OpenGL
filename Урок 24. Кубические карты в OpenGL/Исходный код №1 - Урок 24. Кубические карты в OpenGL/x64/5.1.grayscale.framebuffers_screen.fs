0fe2c620b30fd831d2f27ee98ce2d32c2e6608bd
donButadeus <dezzines@gmail.com>
2020-06-20 21:46:36 +0300

    Новый урок

---
+++ Урок 24. Кубические карты в OpenGL/Исходный код №1 - Урок 24. Кубические карты в OpenGL/x64/5.1.grayscale.framebuffers_screen.fs
@@ -0,0 +1,13 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+uniform sampler2D screenTexture;
+
+void main()
+{
+    FragColor = texture(screenTexture, TexCoords);
+    float average = 0.2126 * FragColor.r + 0.7152 * FragColor.g + 0.0722 * FragColor.b;
+    FragColor = vec4(average, average, average, 1.0);
+}
