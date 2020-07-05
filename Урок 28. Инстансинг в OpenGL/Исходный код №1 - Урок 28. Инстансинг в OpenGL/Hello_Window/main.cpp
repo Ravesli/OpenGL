d3f0c3f481acb9575e6f46722103ef69c8b3d218
donButadeus <dezzines@gmail.com>
2020-07-05 15:23:40 +0300

    Новый урок

---
+++ Урок 28. Инстансинг в OpenGL/Исходный код №1 - Урок 28. Инстансинг в OpenGL/Hello_Window/main.cpp
@@ -0,0 +1,148 @@
+
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "shader.h"
+
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+// настройки
+const unsigned int SCR_WIDTH = 600;
+const unsigned int SCR_HEIGHT = 400;
+
+int main()
+{
+    // glfw: инициализаци€ и конфигурирование
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw создание окна
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL for Ravesli.com!", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // glad: загрузка всех указателей на OpenGL-функции
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // конфигурирование глобального состо€ни€ OpenGL
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+
+    // компилирование нашей шейдерной программы
+    // -------------------------
+    Shader shader("../10.1.instancing.vs", "../10.1.instancing.fs");
+
+    // генерируем список из 100 векторов положени€/трансл€ции пр€моугольников
+    // ---------------------------------------------------------
+    glm::vec2 translations[100];
+    int index = 0;
+    float offset = 0.1f;
+    for (int y = -10; y < 10; y += 2)
+    {
+        for (int x = -10; x < 10; x += 2)
+        {
+            glm::vec2 translation;
+            translation.x = (float)x / 10.0f + offset;
+            translation.y = (float)y / 10.0f + offset;
+            translations[index++] = translation;
+        }
+    }
+
+    // сохран€ем инстанс-данные в буферном массиве
+    // --------------------------------------
+    unsigned int instanceVBO;
+    glGenBuffers(1, &instanceVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * 100, &translations[0], GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // установка вершинных данных (буффера(-ов)) и настройка вершинных атрибутов
+    // ------------------------------------------------------------------
+    float quadVertices[] = {
+        // координаты     // цвет
+        -0.05f,  0.05f,  1.0f, 0.0f, 0.0f,
+         0.05f, -0.05f,  0.0f, 1.0f, 0.0f,
+        -0.05f, -0.05f,  0.0f, 0.0f, 1.0f,
+
+        -0.05f,  0.05f,  1.0f, 0.0f, 0.0f,
+         0.05f, -0.05f,  0.0f, 1.0f, 0.0f,
+         0.05f,  0.05f,  0.0f, 1.0f, 1.0f
+    };
+    unsigned int quadVAO, quadVBO;
+    glGenVertexArrays(1, &quadVAO);
+    glGenBuffers(1, &quadVBO);
+    glBindVertexArray(quadVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
+    // также устанавливаем инстанс-данные
+    glEnableVertexAttribArray(2);
+    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO); // этот атрибут беретс€ из другого буфера вершин
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glVertexAttribDivisor(2, 1); // говорим OpenGL, что это - инстанцированный вершинный атрибут.
+
+
+    // цикл рендеринга
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // рендеринг
+        // ------
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // отрисовываем 100 инстанцированных пр€моугольников
+        shader.use();
+        glBindVertexArray(quadVAO);
+        glDrawArraysInstanced(GL_TRIANGLES, 0, 6, 100); // 100 треугольников по 6 вершин в каждом
+        glBindVertexArray(0);
+
+        // glfw: обмен содержимым переднего и заднего буферов. ќпрос событий ¬вода\¬вывода (была ли нажата/отпущена кнопка, перемещен курсор мыши и т.п.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // опционально: освобеждение пам€ти, выделенной под ресурсы:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &quadVAO);
+    glDeleteBuffers(1, &quadVBO);
+
+    glfwTerminate();
+    return 0;
+}
+
+// glfw: вс€кий раз, когда измен€ютс€ размеры окна (пользователем или опер. системой), вызываетс€ данна€ функци€
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // убеждаемс€, что вьюпорт соответствует новым размерам окна; обратите внимание,
+    // что ширина и высота будут значительно больше, чем указано на retina -диспле€х.
+    glViewport(0, 0, width, height);
+}
+
