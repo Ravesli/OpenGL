2cddfea14e73b81cb47c8c0b9851cbeeb85080b5
donButadeus <dezzines@gmail.com>
2020-07-29 02:48:53 +0300

    Update main.cpp
    Мелкое исправление комментария

--- Урок 31. Гамма-коррекция в OpenGL/Hello_Window/main.cpp
+++ Урок 31. Гамма-коррекция в OpenGL/Hello_Window/main.cpp
@@ -47,7 +47,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
-    // glfw: инициализация и конфигурирование
+    // glfw: создание окна
     // --------------------
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL for Ravesli.com!", NULL, NULL);
     if (window == NULL)
