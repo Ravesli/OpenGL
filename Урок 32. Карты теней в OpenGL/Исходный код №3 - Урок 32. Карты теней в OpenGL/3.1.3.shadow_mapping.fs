8a0c91b9a4ca0fefab68068e59bd436942582963
donButadeus <dezzines@gmail.com>
2020-08-06 16:12:19 +0300

    Новый урок

---
+++ Урок 32. Карты теней в OpenGL/Исходный код №3 - Урок 32. Карты теней в OpenGL/3.1.3.shadow_mapping.fs
@@ -0,0 +1,77 @@
+#version 330 core
+out vec4 FragColor;
+
+in VS_OUT {
+    vec3 FragPos;
+    vec3 Normal;
+    vec2 TexCoords;
+    vec4 FragPosLightSpace;
+} fs_in;
+
+uniform sampler2D diffuseTexture;
+uniform sampler2D shadowMap;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+
+float ShadowCalculation(vec4 fragPosLightSpace)
+{
+    // выполняем деление перспективы
+    vec3 projCoords = fragPosLightSpace.xyz / fragPosLightSpace.w;
+    // трансформируем в диапазон [0,1]
+    projCoords = projCoords * 0.5 + 0.5;
+    // получаем наиболее близкое значение глубины исходя из перспективы глазами источника света (используя в диапазон [0,1] fragPosLight в качестве координат)
+    float closestDepth = texture(shadowMap, projCoords.xy).r; 
+    // получаем глубину текущего фрагмента исходя из перспективы глазами источника света
+    float currentDepth = projCoords.z;
+    // вычисляем смещение (на основе разрешения карты глубины и наклона)
+    vec3 normal = normalize(fs_in.Normal);
+    vec3 lightDir = normalize(lightPos - fs_in.FragPos);
+    float bias = max(0.05 * (1.0 - dot(normal, lightDir)), 0.005);
+    // проверка нахождения текущего фрагмента в тени
+    // float shadow = currentDepth - bias > closestDepth  ? 1.0 : 0.0;
+    // PCF
+    float shadow = 0.0;
+    vec2 texelSize = 1.0 / textureSize(shadowMap, 0);
+    for(int x = -1; x <= 1; ++x)
+    {
+        for(int y = -1; y <= 1; ++y)
+        {
+            float pcfDepth = texture(shadowMap, projCoords.xy + vec2(x, y) * texelSize).r; 
+            shadow += currentDepth - bias > pcfDepth  ? 1.0 : 0.0;        
+        }    
+    }
+    shadow /= 9.0;
+    
+    // оставляем значение тени на уровне 0.0 за границей дальней плоскости пирамиды видимости глазами источника света.
+    if(projCoords.z > 1.0)
+        shadow = 0.0;
+        
+    return shadow;
+}
+
+void main()
+{           
+    vec3 color = texture(diffuseTexture, fs_in.TexCoords).rgb;
+    vec3 normal = normalize(fs_in.Normal);
+    vec3 lightColor = vec3(0.3);
+    // фоновая составляющая
+    vec3 ambient = 0.3 * color;
+    // диффузная составляющая
+    vec3 lightDir = normalize(lightPos - fs_in.FragPos);
+    float diff = max(dot(lightDir, normal), 0.0);
+    vec3 diffuse = diff * lightColor;
+    // отраженная составляющая
+    vec3 viewDir = normalize(viewPos - fs_in.FragPos);
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = 0.0;
+    vec3 halfwayDir = normalize(lightDir + viewDir);  
+    spec = pow(max(dot(normal, halfwayDir), 0.0), 64.0);
+    vec3 specular = spec * lightColor;    
+    // вычисляем тень
+    float shadow = ShadowCalculation(fs_in.FragPosLightSpace);                      
+    vec3 lighting = (ambient + (1.0 - shadow) * (diffuse + specular)) * color;    
+    
+    FragColor = vec4(lighting, 1.0);
+}
+
