fed9b9d53b8667279b93d9473ca24417d4e883ca
donButadeus <dezzines@gmail.com>
2020-11-04 16:22:14 +0300

    Новый урок

---
+++ Урок 44. Отладка/Hello_Window/main.cpp
@@ -0,0 +1,314 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <iostream>
+
+#include "shader.h"
+#include "stb_image.h"
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
+
+// настройки
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+
+GLenum glCheckError_(const char* file, int line)
+{
+    GLenum errorCode;
+    while ((errorCode = glGetError()) != GL_NO_ERROR)
+    {
+        std::string error;
+        switch (errorCode)
+        {
+        case GL_INVALID_ENUM:                  error = "INVALID_ENUM"; break;
+        case GL_INVALID_VALUE:                 error = "INVALID_VALUE"; break;
+        case GL_INVALID_OPERATION:             error = "INVALID_OPERATION"; break;
+        case GL_STACK_OVERFLOW:                error = "STACK_OVERFLOW"; break;
+        case GL_STACK_UNDERFLOW:               error = "STACK_UNDERFLOW"; break;
+        case GL_OUT_OF_MEMORY:                 error = "OUT_OF_MEMORY"; break;
+        case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+        }
+        std::cout << error << " | " << file << " (" << line << ")" << std::endl;
+    }
+    return errorCode;
+}
+#define glCheckError() glCheckError_(__FILE__, __LINE__)
+
+void APIENTRY glDebugOutput(GLenum source,
+    GLenum type,
+    unsigned int id,
+    GLenum severity,
+    GLsizei length,
+    const char* message,
+    const void* userParam)
+{
+    if (id == 131169 || id == 131185 || id == 131218 || id == 131204) return; // игнорируем несущественные коды ошибок
+
+    std::cout << "---------------" << std::endl;
+    std::cout << "Debug message (" << id << "): " << message << std::endl;
+
+    switch (source)
+    {
+    case GL_DEBUG_SOURCE_API:             std::cout << "Source: API"; break;
+    case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System"; break;
+    case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler"; break;
+    case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party"; break;
+    case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application"; break;
+    case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other"; break;
+    } std::cout << std::endl;
+
+    switch (type)
+    {
+    case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+    case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+    case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+    case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+    case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+    case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+    case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+    case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+    case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+    } std::cout << std::endl;
+
+    switch (severity)
+    {
+    case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high"; break;
+    case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium"; break;
+    case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low"; break;
+    case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification"; break;
+    } std::cout << std::endl;
+    std::cout << std::endl;
+}
+
+int main()
+{
+    // glfw: инициализация и конфигурирование
+    // ------------------------------
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true); // закомментируйте эту строку когда будете компилировать релизную версию программы! 
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // glfw: создание окна
+    // --------------------
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL for Ravesli.com!", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    // говорим GLFW захватить курсор нашей мышки
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+    // glad: загрузка всех указателей на OpenGL-функции
+    // ---------------------------------------
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // включаем контекст отладки OpenGL, если контекст позволяет это сделать
+    int flags; glGetIntegerv(GL_CONTEXT_FLAGS, &flags);
+    if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
+    {
+        glEnable(GL_DEBUG_OUTPUT);
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS); // убе;lftvcz, что ошибки отображаются синхронно
+        glDebugMessageCallback(glDebugOutput, nullptr);
+        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+    }
+
+    // конфигурирование глобального состояния OpenGL
+    // -----------------------------
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+
+    // Начальное состояние OpenGL
+    Shader shader("../debugging.vs", "../debugging.fs");
+
+    // конфигурируем 3D куб
+    unsigned int cubeVAO, cubeVBO;
+    float vertices[] = {
+        // задняя грань
+       -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, // нижняя-левая
+        0.5f,  0.5f, -0.5f,  1.0f,  1.0f, // верхняя-правая
+        0.5f, -0.5f, -0.5f,  1.0f,  0.0f, // нижняя-правая         
+        0.5f,  0.5f, -0.5f,  1.0f,  1.0f, // верхняя-правая
+       -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, // нижняя-левая
+       -0.5f,  0.5f, -0.5f,  0.0f,  1.0f, // верхняя-левая
+        // передняя грань
+       -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, // нижняя-левая
+        0.5f, -0.5f,  0.5f,  1.0f,  0.0f, // нижняя-правая
+        0.5f,  0.5f,  0.5f,  1.0f,  1.0f, // верхняя-правая
+        0.5f,  0.5f,  0.5f,  1.0f,  1.0f, // верхняя-правая
+       -0.5f,  0.5f,  0.5f,  0.0f,  1.0f, // верхняя-левая
+       -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, // нижняя-левая
+        // грань слева
+       -0.5f,  0.5f,  0.5f, -1.0f,  0.0f, // верхняя-правая
+       -0.5f,  0.5f, -0.5f, -1.0f,  1.0f, // верхняя-левая
+       -0.5f, -0.5f, -0.5f, -0.0f,  1.0f, // нижняя-левая
+       -0.5f, -0.5f, -0.5f, -0.0f,  1.0f, // нижняя-левая
+       -0.5f, -0.5f,  0.5f, -0.0f,  0.0f, // нижняя-правая
+       -0.5f,  0.5f,  0.5f, -1.0f,  0.0f, // верхняя-правая
+        // грань справа
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f, // верхняя-левая
+        0.5f, -0.5f, -0.5f,  0.0f,  1.0f, // нижняя-правая
+        0.5f,  0.5f, -0.5f,  1.0f,  1.0f, // верхняя-правая         
+        0.5f, -0.5f, -0.5f,  0.0f,  1.0f, // нижняя-правая
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f, // верхняя-левая
+        0.5f, -0.5f,  0.5f,  0.0f,  0.0f, // нижняя-левая     
+        // нижняя грань
+       -0.5f, -0.5f, -0.5f,  0.0f,  1.0f, // верхняя-правая
+        0.5f, -0.5f, -0.5f,  1.0f,  1.0f, // верхняя-левая
+        0.5f, -0.5f,  0.5f,  1.0f,  0.0f, // нижняя-левая
+        0.5f, -0.5f,  0.5f,  1.0f,  0.0f, // нижняя-левая
+       -0.5f, -0.5f,  0.5f,  0.0f,  0.0f, // нижняя-правая
+       -0.5f, -0.5f, -0.5f,  0.0f,  1.0f, // верхняя-правая
+        // верхняя грань
+       -0.5f,  0.5f, -0.5f,  0.0f,  1.0f, // верхняя-левая
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f, // нижняя-правая
+        0.5f,  0.5f, -0.5f,  1.0f,  1.0f, // верхняя-правая     
+        0.5f,  0.5f,  0.5f,  1.0f,  0.0f, // нижняя-правая
+       -0.5f,  0.5f, -0.5f,  0.0f,  1.0f, // верхняя-левая
+       -0.5f,  0.5f,  0.5f,  0.0f,  0.0f  // нижняя-левая        
+    };
+    glGenVertexArrays(1, &cubeVAO);
+    glGenBuffers(1, &cubeVBO);
+    // заполняем буфер
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // связываем вершинные атрибуты
+    glBindVertexArray(cubeVAO);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // загружаем текстуры куба
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    int width, height, nrComponents;
+    unsigned char* data = stbi_load("../resources/textures/wood.png", &width, &height, &nrComponents, 0);
+    if (data)
+    {
+        glTexImage2D(GL_FRAMEBUFFER, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
+    // настраиваем матрицу проекции
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 10.0f);
+    glUniformMatrix4fv(glGetUniformLocation(shader.ID, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+    glUniform1i(glGetUniformLocation(shader.ID, "tex"), 0);
+
+    // цикл рендеринга
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // обработка ввода
+        // -----
+        processInput(window);
+
+        // рендер
+        // ------
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        shader.use();
+        float rotationSpeed = 10.0f;
+        float angle = (float)glfwGetTime() * rotationSpeed;
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, glm::vec3(0.0, 0.0f, -2.5));
+        model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 1.0f, 1.0f));
+        glUniformMatrix4fv(glGetUniformLocation(shader.ID, "model"), 1, GL_FALSE, glm::value_ptr(model));
+
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindVertexArray(cubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
+
+        // glfw: обмен содержимым переднего и заднего буферов. Опрос событий Ввода\Ввывода (была ли нажата/отпущена кнопка, перемещен курсор мыши и т.п.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+// renderQuad() рендерит1x1 XY прямоугольник в NDC
+// -----------------------------------------
+unsigned int quadVAO = 0;
+unsigned int quadVBO;
+void renderQuad()
+{
+    if (quadVAO == 0)
+    {
+        float quadVertices[] = {
+            // координаты        // текстурные координаты
+            -1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+            -1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+             1.0f,  1.0f, 0.0f, 1.0f, 1.0f,
+             1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+        };
+        // задаем VAO плоскости
+        glGenVertexArrays(1, &quadVAO);
+        glGenBuffers(1, &quadVBO);
+        glBindVertexArray(quadVAO);
+        glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), &quadVertices, GL_STATIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+        glEnableVertexAttribArray(1);
+        glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    }
+    glBindVertexArray(quadVAO);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    glBindVertexArray(0);
+}
+
+// Обработка всех событий ввода: запрос GLFW о нажатии/отпускании кнопки мыши в данном кадре и соответствующая обработка данных событий
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+// glfw: всякий раз, когда изменяются размеры окна (пользователем или опер. системой), вызывается данная функция
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    // убеждаемся, что вьюпорт соответствует новым размерам окна; обратите внимание,
+    // что ширина и высота будут значительно больше, чем указано на retina -дисплеях.
+    glViewport(0, 0, width, height);
+}
+
