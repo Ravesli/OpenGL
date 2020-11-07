65a030805c3dcfff3de51f24555268caf7a8af46
donButadeus <dezzines@gmail.com>
2020-11-07 14:59:08 +0300

    Новый урок

---
+++ Урок 47. Setting Up/Hello_Window/game.h
@@ -0,0 +1,42 @@
+/*******************************************************************
+** This code is part of Breakout.
+**
+** Breakout is free software: you can redistribute it and/or modify
+** it under the terms of the CC BY 4.0 license as published by
+** Creative Commons, either version 4 of the License, or (at your
+** option) any later version.
+******************************************************************/
+#ifndef GAME_H
+#define GAME_H
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+// Описывает текущее состояние игры
+enum GameState {
+    GAME_ACTIVE,
+    GAME_MENU,
+    GAME_WIN
+};
+
+// Класс Game содержит все  связанные с игрой параметры состояния и различный функционал. Он объединяет все игровые данные в один класс,
+//обеспечивая тем самым простой доступ к каждому из компонентов класса и //возможность управления ими. 
+class Game
+{
+public:
+    // игровое состояние
+    GameState               State;	
+    bool                    Keys[1024];
+    unsigned int            Width, Height;
+    // конструктор/деструктор
+    Game(unsigned int width, unsigned int height);
+    ~Game();
+    // инициализация начального состояния игры (загрузка всех шейдеров, текстур, уровней)
+    void Init();
+    // игровой цикл
+    void ProcessInput(float dt);
+    void Update(float dt);
+    void Render();
+};
+
+#endif
