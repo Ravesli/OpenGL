8d3fbac1fad6bac0ef24f98064b025466120dde5
donButadeus <dezzines@gmail.com>
2020-11-11 20:51:15 +0300

    Новый урок

---
+++ Часть №3. Игра Breakout на С++ и OpenGL - Рендерим спрайт/Hello_Window/game.cpp
@@ -0,0 +1,58 @@
+/*******************************************************************
+** This code is part of Breakout.
+**
+** Breakout is free software: you can redistribute it and/or modify
+** it under the terms of the CC BY 4.0 license as published by
+** Creative Commons, either version 4 of the License, or (at your
+** option) any later version.
+******************************************************************/
+#include "game.h"
+#include "resource_manager.h"
+#include "sprite_renderer.h"
+
+
+// Данные о состоянии, связанном с игрой
+SpriteRenderer* Renderer;
+
+
+Game::Game(unsigned int width, unsigned int height)
+    : State(GAME_ACTIVE), Keys(), Width(width), Height(height)
+{
+
+}
+
+Game::~Game()
+{
+    delete Renderer;
+}
+
+void Game::Init()
+{
+    // загрузка шейдеров
+    ResourceManager::LoadShader("../shaders/sprite.vs", "../shaders/sprite.frag", nullptr, "sprite");
+    // конфигурирование шейдеров
+    glm::mat4 projection = glm::ortho(0.0f, static_cast<float>(this->Width),
+        static_cast<float>(this->Height), 0.0f, -1.0f, 1.0f);
+    ResourceManager::GetShader("sprite").Use().SetInteger("image", 0);
+    ResourceManager::GetShader("sprite").SetMatrix4("projection", projection);
+    // установка специфичных для рендеринга элементов управления
+    Renderer = new SpriteRenderer(ResourceManager::GetShader("sprite"));
+    // загрузка текстур
+    ResourceManager::LoadTexture("../textures/awesomeface.png", true, "face");
+}
+
+void Game::Update(float dt)
+{
+
+}
+
+void Game::ProcessInput(float dt)
+{
+
+}
+
+void Game::Render()
+{
+    Renderer->DrawSprite(ResourceManager::GetTexture("face"), glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+}
+
