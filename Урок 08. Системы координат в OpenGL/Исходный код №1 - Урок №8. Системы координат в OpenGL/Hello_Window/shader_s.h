e8b042c3f93c28ebcbb01b5588970675fb29a170
Yura <li4er441@gmail.com>
2020-12-24 20:35:03 +0200

    Обновление комментариев

--- Урок 08. Системы координат в OpenGL/Исходный код №1 - Урок №8. Системы координат в OpenGL/Hello_Window/shader_s.h
+++ Урок 08. Системы координат в OpenGL/Исходный код №1 - Урок №8. Системы координат в OpenGL/Hello_Window/shader_s.h
@@ -13,37 +13,41 @@ class Shader
 {
 public:
     unsigned int ID;
-    // конструктор генерирует шейдер на лету
-    // ------------------------------------------------------------------------
+    // Конструктор генерирует шейдер "на лету"
     Shader(const char* vertexPath, const char* fragmentPath, const char* geometryPath = nullptr)
     {
-        // 1. получение исходного кода вершинного/фрагментного шейдера
+        // 1. Получение исходного кода вершинного/фрагментного шейдера
         std::string vertexCode;
         std::string fragmentCode;
         std::string geometryCode;
         std::ifstream vShaderFile;
         std::ifstream fShaderFile;
         std::ifstream gShaderFile;
-        // убеждаемся, что объекты ifstream могут выбросить исключение:
+		
+        // Убеждаемся, что объекты ifstream могут выбросить исключение:
         vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
         fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
         gShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
         try
         {
-            // открываем файлы
+            // Открываем файлы
             vShaderFile.open(vertexPath);
             fShaderFile.open(fragmentPath);
             std::stringstream vShaderStream, fShaderStream;
-            // читаем содержимое файловых буферов
+			
+            // Читаем содержимое файловых буферов
             vShaderStream << vShaderFile.rdbuf();
             fShaderStream << fShaderFile.rdbuf();
-            // закрываем файлы
+			
+            // Закрываем файлы
             vShaderFile.close();
             fShaderFile.close();
-            // конвертируем в строковую переменную данные из потока
+			
+            // Конвертируем в строковую переменную данные из потока
             vertexCode = vShaderStream.str();
             fragmentCode = fShaderStream.str();
-            // if geometry shader path is present, also load a geometry shader
+			
+            // Если путь геометрического шейдера присутствует, то также загружаем геометрический шейдер
             if (geometryPath != nullptr)
             {
                 gShaderFile.open(geometryPath);
@@ -59,18 +63,22 @@ public:
         }
         const char* vShaderCode = vertexCode.c_str();
         const char* fShaderCode = fragmentCode.c_str();
-        // 2. компилируем шейдеры
+		
+        // 2. Компилируем шейдеры
         unsigned int vertex, fragment;
-        // вершинный шейдер
+		
+        // Вершинный шейдер
         vertex = glCreateShader(GL_VERTEX_SHADER);
         glShaderSource(vertex, 1, &vShaderCode, NULL);
         glCompileShader(vertex);
         checkCompileErrors(vertex, "VERTEX");
+		
         // Фрагментный шейдер
         fragment = glCreateShader(GL_FRAGMENT_SHADER);
         glShaderSource(fragment, 1, &fShaderCode, NULL);
         glCompileShader(fragment);
         checkCompileErrors(fragment, "FRAGMENT");
+		
         // Если был дан геометрический шейдер, то компилируем его
         unsigned int geometry;
         if (geometryPath != nullptr)
@@ -81,6 +89,7 @@ public:
             glCompileShader(geometry);
             checkCompileErrors(geometry, "GEOMETRY");
         }
+		
         // Шейдерная программа
         ID = glCreateProgram();
         glAttachShader(ID, vertex);
@@ -89,35 +98,31 @@ public:
             glAttachShader(ID, geometry);
         glLinkProgram(ID);
         checkCompileErrors(ID, "PROGRAM");
-        // После того, как мы связали шейдеры с нашей программой, удаляем их, т.к. они больше не нужны
+		
+        // После того, как мы связали шейдеры с нашей программой, удаляем их, т.к. они нам больше не нужны
         glDeleteShader(vertex);
         glDeleteShader(fragment);
         if (geometryPath != nullptr)
             glDeleteShader(geometry);
     }
-    // активация шейдера
-    // ------------------------------------------------------------------------
+    // Активация шейдера
     void use() const
     {
         glUseProgram(ID);
     }
-    // полезные uniform-функции
-    // ------------------------------------------------------------------------
+    // Полезные uniform-функции
     void setBool(const std::string& name, bool value) const
     {
         glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
     }
-    // ------------------------------------------------------------------------
     void setInt(const std::string& name, int value) const
     {
         glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
     }
-    // ------------------------------------------------------------------------
     void setFloat(const std::string& name, float value) const
     {
         glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
     }
-    // ------------------------------------------------------------------------
     void setVec2(const std::string& name, const glm::vec2& value) const
     {
         glUniform2fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
@@ -126,7 +131,6 @@ public:
     {
         glUniform2f(glGetUniformLocation(ID, name.c_str()), x, y);
     }
-    // ------------------------------------------------------------------------
     void setVec3(const std::string& name, const glm::vec3& value) const
     {
         glUniform3fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
@@ -135,7 +139,6 @@ public:
     {
         glUniform3f(glGetUniformLocation(ID, name.c_str()), x, y, z);
     }
-    // ------------------------------------------------------------------------
     void setVec4(const std::string& name, const glm::vec4& value) const
     {
         glUniform4fv(glGetUniformLocation(ID, name.c_str()), 1, &value[0]);
@@ -144,25 +147,21 @@ public:
     {
         glUniform4f(glGetUniformLocation(ID, name.c_str()), x, y, z, w);
     }
-    // ------------------------------------------------------------------------
     void setMat2(const std::string& name, const glm::mat2& mat) const
     {
         glUniformMatrix2fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
     }
-    // ------------------------------------------------------------------------
     void setMat3(const std::string& name, const glm::mat3& mat) const
     {
         glUniformMatrix3fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
     }
-    // ------------------------------------------------------------------------
     void setMat4(const std::string& name, const glm::mat4& mat) const
     {
         glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
     }
 
 private:
-    // полезные функции для проверки ошибок компиляции/связывания шейдеров.
-    // ------------------------------------------------------------------------
+    // Полезные функции для проверки ошибок компиляции/связывания шейдеров
     void checkCompileErrors(GLuint shader, std::string type)
     {
         GLint success;
--- Урок 08. Системы координат в OpenGL/Исходный код №3 - Урок №8. Системы координат в OpenGL/Hello_Window/main.cpp
+++ Урок 08. Системы координат в OpenGL/Исходный код №3 - Урок №8. Системы координат в OpenGL/Hello_Window/main.cpp
@@ -1,5 +1,3 @@
-
-
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "stb_image.h"
@@ -22,18 +20,16 @@ const unsigned int SCR_HEIGHT = 600;
 int main()
 {
     // glfw: инициализация и конфигурирование
-    // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Расскоментировать строчку в случае использования MacOS X
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // раскомментируйте эту строку, если используете macOS
 #endif
 
-    // glfw создание окна
-    // --------------------
+    // glfw: создание окна
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL for Ravesli.com", NULL, NULL);
     if (window == NULL)
     {
@@ -45,25 +41,21 @@ int main()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
     // glad: загрузка всех указателей на OpenGL-функции
-    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // конфигурирование глобального состояния OpenGL
-    // -----------------------------
+    // Конфигурирование глобального состояния OpenGL
     glEnable(GL_DEPTH_TEST);
 
-    // компилирование нашей шейдерной программы
-    // ------------------------------------
+    // Компилирование нашей шейдерной программы
     Shader ourShader("../6.2.coordinate_systems.vs", "../6.2.coordinate_systems.fs");
 
-    // задание вершин (и буфера(ов)) и настройка вершинных атрибутов
-    // ------------------------------------------------------------------
+    // Указание вершин (и буфера(ов)) и настройка вершинных атрибутов
     float vertices[] = {
-        // координаты          // текстурные координаты
+          // координаты        // текстурные координаты
          -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
           0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
           0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
@@ -123,30 +115,32 @@ int main()
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // координатные атрибуты
+    // Координатные атрибуты
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // цветовые атрибуты
+    // Цветовые атрибуты
     glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
 
-    // загрузка и создание текстуры
-    // -------------------------
+    // Загрузка и создание текстур
     unsigned int texture1, texture2;
-    // Текстура 1 - Деревянный ящик
-    // ---------
+	
+    // Текстура №1 - Деревянный ящик
     glGenTextures(1, &texture1);
     glBindTexture(GL_TEXTURE_2D, texture1);
-    // установка параметров наложения текстуры
+	
+    // Установка параметров наложения текстуры
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // установка параметров фильтрации текстуры
+	
+    // Установка параметров фильтрации текстуры
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // загрузка изображения, создание текстуры и генерирование mipmap-уровней
+	
+    // Загрузка изображения, создание текстуры и генерирование мипмап-уровней
     int width, height, nrChannels;
-    stbi_set_flip_vertically_on_load(true); // указываем stb_image.h на то, чтобы перевернуть для загруженной текстуры ось Y.
+    stbi_set_flip_vertically_on_load(true); // указываем stb_image.h на то, чтобы перевернуть для загруженной текстуры ось y
     unsigned char* data = stbi_load("../textures/wooden_container.jpg", &width, &height, &nrChannels, 0);
     if (data)
     {
@@ -158,21 +152,24 @@ int main()
         std::cout << "Failed to load texture" << std::endl;
     }
     stbi_image_free(data);
-    // Текстура 2 - Смайлик
-    // ---------
+	
+    // Текстура №2 - Смайлик
     glGenTextures(1, &texture2);
     glBindTexture(GL_TEXTURE_2D, texture2);
-    // установка параметров наложения текстуры
+	
+    // Установка параметров наложения текстуры
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    // установка параметров фильтрации текстуры
+	
+    // Установка параметров фильтрации текстуры
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    // загрузка изображения, создание текстуры и генерирование mipmap-уровней
+	
+    // Загрузка изображения, создание текстуры и генерирование мипмап-уровней
     data = stbi_load("../textures/awesomeface.png", &width, &height, &nrChannels, 0);
     if (data)
     {
-        // файл awesomeface.png имеет альфа-канал(прозрачность), поэтому необходимо использовать пераметр GL_RGBA
+        // Файл awesomeface.png имеет альфа-канал (прозрачность), поэтому необходимо использовать пераметр GL_RGBA
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
@@ -182,87 +179,79 @@ int main()
     }
     stbi_image_free(data);
 
-    // указываем opengl какой сэмплер к какому текстурному блоку принадлежит (это нужно сделать только один раз) 
-    // -------------------------------------------------------------------------------------------
+    // Указываем OpenGL, какой сэмплер к какому текстурному блоку принадлежит (это нужно сделать единожды)
     ourShader.use();
     ourShader.setInt("texture1", 0);
     ourShader.setInt("texture2", 1);
 
 
-    // цикл рендеринга
-    // -----------
+    // Цикл рендеринга
     while (!glfwWindowShouldClose(window))
     {
-        // обработка ввода
-        // -----
+        // Обработка ввода
         processInput(window);
 
-        // рендеринг
-        // ------
+        // Рендеринг
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Очищаем буфер цвета и буфер глубины
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // очищаем буфер цвета и буфер глубины
 
-        // привязка текстур к соответствующим текстурным юнитам
+        // Привязка текстур к соответствующим текстурным юнитам
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, texture1);
         glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, texture2);
 
-        //Активируем шейдер
+        // Активируем шейдер
         ourShader.use();
 
-        // создаем преобразование
+        // Создаем преобразование
         glm::mat4 model = glm::mat4(1.0f); // сначала инициализируем единичную матрицу
         glm::mat4 view = glm::mat4(1.0f);
         glm::mat4 projection = glm::mat4(1.0f);
         model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        // получаем местоположение uniform-матриц...
+		
+        // Получаем местоположение uniform-матриц...
         unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
         unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
-        // ...передаём их в шейдеры(разными способами)
+        // ...передаем их в шейдеры (разными способами)
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
-        //Примечание: В настоящее время мы устанавливаем матрицу проекции для каждого кадра, но поскольку матрица проекции редко меняется, то рекомендуется устанавливать ее (единожды) вне основного цикла.
+		
+        // Примечание: В настоящее время мы устанавливаем матрицу проекции для каждого кадра, но поскольку матрица проекции редко меняется, то рекомендуется устанавливать её (единожды) вне основного цикла
         ourShader.setMat4("projection", projection);
 
-        // рендерим ящик
+        // Рендерим ящик
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
-        // glfw: обмен содержимым переднего и заднего буферов. Опрос событий Ввода\Ввывода (была ли нажата/отпущена кнопка, перемещен курсор мыши и т.п.)
-        // -------------------------------------------------------------------------------
+        // glfw: обмен содержимым front- и back- буферов. Отслеживание событий ввода\вывода (была ли нажата/отпущена кнопка, перемещен курсор мыши и т.п.)
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
-    // опционально: освобождаем все ресурсы, как только они изжили своё назначение:
-    // ------------------------------------------------------------------------
+    // Опционально: освобождаем все ресурсы, как только они выполнили свое предназначение
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
     glDeleteBuffers(1, &EBO);
 
-    // glfw: завершение, освобождение всех выделенных ранее GLFW-реурсов.
-    // ------------------------------------------------------------------
+    // glfw: завершение, освобождение всех выделенных ранее GLFW-ресурсов
     glfwTerminate();
     return 0;
 }
 
 // Обработка всех событий ввода: запрос GLFW о нажатии/отпускании кнопки мыши в данном кадре и соответствующая обработка данных событий
-// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
 }
 
-// glfw: всякий раз, когда изменяются размеры окна (пользователем или опер. системой), вызывается данная функция
-// ---------------------------------------------------------------------------------------------
+// glfw: всякий раз, когда изменяются размеры окна (пользователем или операционной системой), вызывается данная callback-функция
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // убеждаемся, что вьюпорт соответствует новым размерам окна; обратите внимание,
-    // что ширина и высота будут значительно больше, чем указано на retina -дисплеях.
+    // Убеждаемся, что окно просмотра соответствует новым размерам окна.
+    // Обратите внимание, высота и ширина будут значительно больше, чем указано, на Retina-дисплеях
     glViewport(0, 0, width, height);
 }
-
