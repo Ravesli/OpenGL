3ba166f6a67630933d2462993ff59a83314b836f
Yura <li4er441@gmail.com>
2020-12-29 02:11:02 +0200

    Обновление комментариев

--- Урок 13. Карты освещения в OpenGL/Исходный код №1 - Урок №13. Карты освещения в OpenGL/Hello_Window/main.cpp
+++ Урок 13. Карты освещения в OpenGL/Исходный код №1 - Урок №13. Карты освещения в OpenGL/Hello_Window/main.cpp
@@ -37,18 +37,16 @@ glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
 int main()
 {
     // glfw: инициализация и конфигурирование
-    // ------------------------------
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // раскомментируйте эту строку, если используете macOS
 #endif
 
-    // glfw создание окна
-    // --------------------
+    // glfw: создание окна
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "OpenGL for Ravesli.com", NULL, NULL);
     if (window == NULL)
     {
@@ -61,30 +59,26 @@ int main()
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
 
-    // говорим GLFW чтобы он захватил нашу мышь
+    // Сообщаем GLFW, чтобы он захватил наш курсор
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
     // glad: загрузка всех указателей на OpenGL-функции
-    // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // конфигурирование глобального состояния OpenGL
-    // -----------------------------
+    // Конфигурирование глобального состояния OpenGL
     glEnable(GL_DEPTH_TEST);
 
-    // компилирование нашей шейдерной программы
-    // ------------------------------------
+    // Компилирование нашей шейдерной программы
     Shader lightingShader("../4.1.lighting_maps.vs", "../4.1.lighting_maps.fs");
     Shader lightCubeShader("../4.1.light_cube.vs", "../4.1.light_cube.fs");
 
-    // задание вершин (и буфера(ов)) и настройка вершинных атрибутов
-    // ------------------------------------------------------------------
+    // Указание вершин (и буфера(ов)) и настройка вершинных атрибутов
     float vertices[] = {
-        // координаты          // нормали           // текстурные координаты
+         // координаты        // нормали           // текстурные координаты
         -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  0.0f, 0.0f,
          0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 0.0f,
          0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,  1.0f, 1.0f,
@@ -128,7 +122,7 @@ int main()
         -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,  0.0f, 1.0f
     };
 
-// 1. настраиваем VAO (и VBO) куба
+// 1. Настраиваем VAO (и VBO) куба
 unsigned int VBO, cubeVAO;
 glGenVertexArrays(1, &cubeVAO);
 glGenBuffers(1, &VBO);
@@ -144,111 +138,100 @@ glEnableVertexAttribArray(1);
 glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 glEnableVertexAttribArray(2);
 
-//2. настраиваем VAO света (VBO остается неизменным; вершины те же и для светового объекта, который также является 3D-кубом)
+// 2. Настраиваем VAO света (VBO остается неизменным; вершины те же и для светового объекта, который также является 3D-кубом)
 unsigned int lightCubeVAO;;
 glGenVertexArrays(1, &lightCubeVAO);
 glBindVertexArray(lightCubeVAO);
 
 glBindBuffer(GL_ARRAY_BUFFER, VBO);
 
-// обратите внимание, что мы обновляем шаг атрибута положения лампы, чтобы отразить обновленные данные буфера
+// Обратите внимание, что мы обновляем шаг атрибута положения лампы, чтобы отразить обновленные данные буфера
 glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 glEnableVertexAttribArray(0);
 
-//загрузка текстур
-// -----------------------------------------------------------------------------
+// Загрузка текстур
 unsigned int diffuseMap = loadTexture("../textures/wooden_container_2.png");
-// конфигурация шейдеров
-// --------------------
+
+// Конфигурация шейдеров
 lightingShader.use();
 lightingShader.setInt("material.diffuse", 0);
 
-// цикл рендеринга
-// -----------
+// Цикл рендеринга
 while (!glfwWindowShouldClose(window))
 {
-    // логическая часть работы со временем для каждого кадра
-    // --------------------
+    // Логическая часть работы со временем для каждого кадра
     float currentFrame = glfwGetTime();
     deltaTime = currentFrame - lastFrame;
     lastFrame = currentFrame;
 
-    // обработка ввода
-    // -----
+    // Обработка ввода
     processInput(window);
 
-    // рендеринг
-    // ------
+    // Рендеринг
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    // убеждаемся, что активировали шейдер прежде, чем настраивать uniform-переменные/объекты_рисования
+    // Убеждаемся, что активировали шейдер прежде, чем настраивать uniform-переменные/объекты_рисования
     lightingShader.use();
     lightingShader.setVec3("light.position", lightPos);
     lightingShader.setVec3("viewPos", camera.Position);
 
-    // свойства света
+    // Свойства света
     lightingShader.setVec3("light.ambient", 0.2f, 0.2f, 0.2f);
     lightingShader.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
     lightingShader.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
 
-    // свойства материалов
+    // Свойства материалов
     lightingShader.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
     lightingShader.setFloat("material.shininess", 64.0f);
 
-
-    // преобразования Вида/Проекции
+    // Преобразования Вида/Проекции
     glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
     glm::mat4 view = camera.GetViewMatrix();
     lightingShader.setMat4("projection", projection);
     lightingShader.setMat4("view", view);
 
-    // мировое преобразование
+    // Мировое преобразование
     glm::mat4 model = glm::mat4(1.0f);
     lightingShader.setMat4("model", model);
 
-    // связывание диффузной карты
+    // Связывание диффузной карты
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, diffuseMap);
 
-    // рендеринг куба
+    // Рендеринг куба
     glBindVertexArray(cubeVAO);
     glDrawArrays(GL_TRIANGLES, 0, 36);
 
-
-    // также отрисовываем наш объект-"лампочку" 
+    // Также отрисовываем наш объект-"лампочку" 
     lightCubeShader.use();
     lightCubeShader.setMat4("projection", projection);
     lightCubeShader.setMat4("view", view);
     model = glm::mat4(1.0f);
     model = glm::translate(model, lightPos);
-    model = glm::scale(model, glm::vec3(0.2f)); // куб, меньшего размера
+    model = glm::scale(model, glm::vec3(0.2f)); // куб меньшего размера
     lightCubeShader.setMat4("model", model);
 
     glBindVertexArray(lightCubeVAO);
     glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
-    // glfw: обмен содержимым переднего и заднего буферов. Опрос событий Ввода\Ввывода (была ли нажата/отпущена кнопка, перемещен курсор мыши и т.п.)
-    // -------------------------------------------------------------------------------
+    // glfw: обмен содержимым front- и back- буферов. Отслеживание событий ввода/вывода (была ли нажата/отпущена кнопка, перемещен курсор мыши и т.п.)
     glfwSwapBuffers(window);
     glfwPollEvents();
 }
 
-// опционально: освобождаем все ресурсы, как только они изжили своё назначение:
-// ------------------------------------------------------------------------
+// Опционально: освобождаем все ресурсы, как только они выполнили свое предназначение
 glDeleteVertexArrays(1, &cubeVAO);
 glDeleteVertexArrays(1, &lightCubeVAO);
 glDeleteBuffers(1, &VBO);
 
-// glfw: завершение, освобождение всех выделенных ранее GLFW-реурсов.
-// ------------------------------------------------------------------
+// glfw: завершение, освобождение всех выделенных ранее GLFW-реcурсов
 glfwTerminate();
 return 0;
 }
 
 // Обработка всех событий ввода: запрос GLFW о нажатии/отпускании кнопки мыши в данном кадре и соответствующая обработка данных событий
-// ---------------------------------------------------------------------------------------------------------
 void processInput(GLFWwindow* window)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
@@ -264,18 +247,16 @@ void processInput(GLFWwindow* window)
         camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
-// glfw: всякий раз, когда изменяются размеры окна (пользователем или опер. системой), вызывается данная функция
-// ---------------------------------------------------------------------------------------------
+// glfw: всякий раз, когда изменяются размеры окна (пользователем или операционной системой), вызывается данная callback-функция
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-    // убеждаемся, что вьюпорт соответствует новым размерам окна; обратите внимание,
-    // что ширина и высота будут значительно больше, чем указано на retina -дисплеях.
+    // Убеждаемся, что окно просмотра соответствует новым размерам окна.
+    // Обратите внимание, ширина и высота будут значительно больше, чем указано, на Retina-дисплеях
     glViewport(0, 0, width, height);
 }
 
 
 // glfw: всякий раз, когда перемещается мышь, вызывается данная callback-функция
-// -------------------------------------------------------
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
     if (firstMouse)
@@ -286,7 +267,7 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
     }
 
     float xoffset = xpos - lastX;
-    float yoffset = lastY - ypos; // перевернуто, так как Y-координаты идут снизу вверх
+    float yoffset = lastY - ypos; // перевернуто, так как y-координаты идут снизу вверх
 
     lastX = xpos;
     lastY = ypos;
@@ -295,13 +276,12 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 }
 
 // glfw: всякий раз, когда прокручивается колесико мыши, вызывается данная callback-функция
-// ----------------------------------------------------------------------
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
     camera.ProcessMouseScroll(yoffset);
 }
+
 // Функция для загрузки 2D-текстуры из файла
-// ---------------------------------------------------
 unsigned int loadTexture(char const* path)
 {
     unsigned int textureID;
--- Урок 13. Карты освещения в OpenGL/Исходный код №2 - Урок №13. Карты освещения в OpenGL/x64/4.2.lighting_maps.fs
+++ Урок 13. Карты освещения в OpenGL/Исходный код №2 - Урок №13. Карты освещения в OpenGL/x64/4.2.lighting_maps.fs
@@ -25,16 +25,16 @@ uniform Light light;
 
 void main()
 {
-    // ambient
+    // Окружающая составляющая
     vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
   	
-    // diffuse 
+    // Диффузная составляющая 
     vec3 norm = normalize(Normal);
     vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
     
-    // specular
+    // Отраженная составляющая
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
